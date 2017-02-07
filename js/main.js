3ae57563ec4767952131dd8505cb25aca7dbdb29
luchenxu007 <977865207@qq.com>
2017-02-07 16:01:52 +0800

    新建

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// 实例图片广告的插件
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		// 添加配置项具体查看官方文档
+		items:1,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:1000,
+		autoplayHoverPause:true
+	});
+	
+});
